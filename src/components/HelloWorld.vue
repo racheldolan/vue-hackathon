83a9b14711c99b87ef6483053090dcbeb27a1068
Rachel Dolan <racheldolan1@gmail.com>
2018-07-23 14:12:38 +0100

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+<h1>yo</h1>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'yo'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
