cdab42fd03b4297f675adaa9db39aa3000fed6f7
Stevan Popovic <stevanpopovic5@gmail.com>
2018-07-23 15:30:15 +0100

    Got data outputting

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,35 @@
 <template>
-<h1>yo</h1>
+  <section>
+    <h1>yo</h1>
+    <ul>
+      <li v-for="bike in bikeData">
+        {{bike.commonName}} | <strong> {{bike.lat}}, {{bike.lon}} </strong>
+      </li>
+    </ul>
+
+  </section>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'HelloWorld',
   data () {
-    return {
-      msg: 'yo'
+  return {
+      msg: 'yo',
+      bikeData: []
     }
+  },
+  mounted () {
+    axios({
+      method: 'GET',
+      url: 'https://api.tfl.gov.uk/bikepoint'
+    })
+      .then(res => this.bikeData = res.data)
+      .catch(err => console.log(err))
   }
+
 }
 </script>
 
